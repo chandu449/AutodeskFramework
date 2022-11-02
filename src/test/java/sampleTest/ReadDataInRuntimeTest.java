e50f89cb41309de5efa5c4ad42d687c08bcd06b8
admin <admin@DESKTOP-QAGKNHM>
2022-11-02 19:56:32 +0530

    This push is from Sample Repo for "ReadDataInRuntimeTest" method

--- src/test/java/sampleTest/ReadDataInRuntimeTest.java
+++ src/test/java/sampleTest/ReadDataInRuntimeTest.java
@@ -9,9 +9,13 @@ public class ReadDataInRuntimeTest {
 	{
 		String BROWSER = System.getProperty("browser");
 		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
 		
 		System.out.println(BROWSER);
 		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 
 }
