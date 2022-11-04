7e3b98bf5bc591491280109a9d3f191312cad18b
admin <admin@DESKTOP-QAGKNHM>
2022-11-04 20:48:56 +0530

    Updated for Poll SCM

---
+++ src/test/java/sampleTest/PollTest.java
@@ -0,0 +1,14 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+public class PollTest {
+	
+	@Test
+	public void scm()
+	{
+		System.out.println("changes are done ");
+		System.out.println("changes are done ");
+	}
+
+}
